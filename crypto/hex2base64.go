98c64f8dfa5bb129e2590b9b2aeac41f42ebe1f3
Stefan Geneshky <stefan.geneshky@capitalone.com>
2014-08-12 17:33:43 -0700

    Add challenge 01.02

--- crypto/hex2base64.go
+++ crypto/hex2base64.go
@@ -1,17 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "encoding/base64"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
+	"fmt"
 )
 
+func hex2base64(h string) (string, error) {
+	s, err := hex.DecodeString(h)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(s), nil
+}
+
+func fixedXor(xs1, xs2 string) (string, error) {
+	if len(xs1) != len(xs2) {
+		return "", errors.New("Strings with different length")
+	}
+
+	s1, err := hex.DecodeString(xs1)
+	if err != nil {
+		return "", err
+	}
+
+	s2, err := hex.DecodeString(xs2)
+	if err != nil {
+		return "", err
+	}
+
+	res := make([]byte, len(s1))
+	for i := 0; i < len(s1); i++ {
+		res[i] = s1[i] ^ s2[i]
+	}
+
+	return hex.EncodeToString(res), nil
+}
+
 func main() {
-    hex := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    s := make([]byte, len(hex) / 2)
-    for i := 0; i < len(hex) / 2; i++ {
-        val, _ := strconv.ParseInt(hex[i * 2:i * 2 + 2], 16, 8)
-        s[i] = byte(val)
-    }
-    fmt.Println(base64.StdEncoding.EncodeToString(s))
+	s, _ := fixedXor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+	fmt.Println(s)
 }
