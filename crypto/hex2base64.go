81f5084c72fc0f01c2418247b59f76339a897206
Stefan Geneshky <stefan.geneshky@capitalone.com>
2014-08-13 13:34:11 -0700

    Complete 01.03

--- crypto/hex2base64.go
+++ crypto/hex2base64.go
@@ -5,7 +5,22 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"sort"
 )
+
+/* Letters type is used to sort a map   */
+type Letter struct {
+	key   byte
+	value byte
+}
+
+type Letters []Letter
+
+func (l Letters) Len() int           { return len(l) }
+func (l Letters) Less(i, j int) bool { return l[i].value > l[j].value }
+func (l Letters) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+
+/****************************************/
 
 func hex2base64(h string) (string, error) {
 	s, err := hex.DecodeString(h)
@@ -38,7 +53,41 @@ func fixedXor(xs1, xs2 string) (string, error) {
 	return hex.EncodeToString(res), nil
 }
 
+func singleByteXor(xs string) (string, byte) {
+	s, err := hex.DecodeString(xs)
+	if err != nil {
+		panic(err)
+	}
+
+	// Count the letter occurences
+	m := make(map[byte]byte)
+	for _, char := range s {
+		m[byte(char)]++
+	}
+
+	// Move all keys and values in an array
+	letters := make(Letters, 0, len(s))
+	for key, value := range m {
+		letters = append(letters, Letter{key, value})
+	}
+
+	// Sort that array by value (reverse)
+	sort.Sort(letters)
+
+	// The most used symbol is the space.
+	// XOR the most used key with it to find the code
+	code := letters[0].key ^ ' '
+
+	// XOR the entire array with the code
+	for i, _ := range s {
+		s[i] ^= code
+	}
+
+	return string(s), code
+}
+
 func main() {
-	s, _ := fixedXor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+	s, code := singleByteXor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 	fmt.Println(s)
+	fmt.Printf("%x\n", code)
 }
